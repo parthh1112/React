7f064af41d315aff86ddae255cbed94b42615c97
parth gupta <parthgupta0824@gmail.com>
2023-12-25 01:25:33 +0530

    video 3

---
+++ 01basicreactjs_create_react_app/src/Parth.js
@@ -0,0 +1,7 @@
+function Parth(){
+    return (
+        <h1>Parth in create react app</h1>
+    )
+}
+ 
+export default Parth;
---
+++ 02viteReact/src/parth.jsx
@@ -0,0 +1,9 @@
+
+ 
+function Parth() {
+    return (
+        <h2>Parth h2</h2>
+    )
+}
+
+export default Parth
