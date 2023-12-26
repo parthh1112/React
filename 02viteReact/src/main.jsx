c9f300efe4a5738d5235faedbfe4b11e03a243da
parth gupta <parthgupta0824@gmail.com>
2023-12-26 13:26:29 +0530

    03customReact

--- 02viteReact/src/main.jsx
+++ 02viteReact/src/main.jsx
@@ -3,9 +3,32 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Parth from './parth.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-    {/* < App /> */}
-    < Parth />
-  </>
+
+
+function MyApp() {
+  return (
+    <div>
+      <h1>This is a custom app</h1>
+    </div>
+  )
+}
+
+const reactElement = React.createElement(
+  'a',
+  {href:'https://google.com',target:'_blank'},
+  'click google is here '
 )
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).
+render(
+  reactElement
+    // // MyApp()
+    // <>
+    //   < MyApp />
+    //   {/* or */}
+    //   {/* < Parth /> */}
+    // </>
+  )
