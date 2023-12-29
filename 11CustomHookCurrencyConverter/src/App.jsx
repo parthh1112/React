a55e55d9fbb572c1abf728f997983efadaa402c8
parth gupta <parthgupta0824@gmail.com>
2023-12-30 00:46:13 +0530

    currencyConverter

---
+++ 11CustomHookCurrencyConverter/src/App.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+
+import { useState } from 'react'
+function App() {
+// https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/inr.json
+  return (
+    <>
+      <div className='w-screen h-screen bg-custom-bg flex justify-center  items-center  '>
+        <div className='center-main-box w-[40%] h-[55%] bg-green-800'>
+
+        </div>
+
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ 11CustomHookCurrencyConverter/src/customHook/useCurrencyInfo.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+
+import { useEffect,useState } from "react";
+
+function useCurrencyInfo(currency){
+    
+
+
+}
